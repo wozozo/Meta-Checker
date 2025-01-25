f3c498814bc427d83c077eeda23290b8c356d015
Yoichi Fujimoto <wozozo@gmail.com>
2025-01-25 18:01:56 +0900

    Refactor notification handling and improve meta tag message generation

--- bg.js
+++ bg.js
@@ -3,14 +3,21 @@
  * @url: http://richard.parnaby-king.co.uk
  */
 const notifications = {};
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+
+// Register click handler for notifications once
+chrome.notifications.onClicked.addListener((notificationId) => {
+  chrome.tabs.update(notifications[notificationId], { active: true });
+});
+
+// Function to create notification
+function createMetaCheckNotification(message, sender) {
   chrome.notifications.create(
     "", //create new notification id
     {
       type: "basic",
       iconUrl: "meta-fail.png",
       title: "Meta Robots Checker",
-      message: msg,
+      message: message,
       contextMessage: sender.tab.title,
       priority: 0,
       isClickable: true,
@@ -19,7 +26,9 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
       notifications[notificationId] = sender.tab.id;
     },
   );
-  chrome.notifications.onClicked.addListener((notificationId) => {
-    chrome.tabs.update(notifications[notificationId], { active: true });
-  });
+}
+
+// Message listener
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  createMetaCheckNotification(msg, sender);
 });
--- contentscript.js
+++ contentscript.js
@@ -3,28 +3,46 @@
  * @url: http://richard.parnaby-king.co.uk
  */
 
-// Get all meta tags
-const metaTags = document.getElementsByTagName("meta");
+const MESSAGES = {
+  NOINDEX_NOFOLLOW: "It looks like this page has a noindex,nofollow. This means google will not crawl the website or index it",
+  NOINDEX: "It looks like this page has a noindex. This means google will not index it",
+  NOFOLLOW: "It looks like this page has a nofollow. This means google will not crawl the website"
+};
 
-// Check meta tags for robots directives
-for (const metaTag of metaTags) {
-  if (metaTag.getAttribute("name") === "robots") {
-    const content = metaTag.getAttribute("content").toLowerCase();
-    let msg = "";
+// Check meta tag content and return message
+function checkMetaContent(metaContent) {
+  const content = metaContent.toLowerCase();
 
-    if (content.includes("noindex") && content.includes("nofollow")) {
-      msg =
-        "It looks like this page has a noindex,nofollow. This means google will not crawl the website or index it";
-    } else if (content.includes("noindex")) {
-      msg =
-        "It looks like this page has a noindex. This means google will not index it";
-    } else if (content.includes("nofollow")) {
-      msg =
-        "It looks like this page has a nofollow. This means google will not crawl the website";
-    }
+  if (content.includes("noindex") && content.includes("nofollow")) {
+    return MESSAGES.NOINDEX_NOFOLLOW;
+  }
 
-    if (msg) {
-      chrome.runtime.sendMessage(msg);
+  if (content.includes("noindex")) {
+    return MESSAGES.NOINDEX;
+  }
+
+  if (content.includes("nofollow")) {
+    return MESSAGES.NOFOLLOW;
+  }
+
+  return null;
+}
+
+// Check meta tags and send notification
+function checkRobotsMetaTag() {
+  const metaTags = document.getElementsByTagName("meta");
+
+  for (const metaTag of metaTags) {
+    if (metaTag.getAttribute("name") === "robots") {
+      const content = metaTag.getAttribute("content");
+      const message = checkMetaContent(content);
+
+      if (message) {
+        chrome.runtime.sendMessage(message);
+      }
     }
   }
 }
+
+// Execute
+checkRobotsMetaTag();
