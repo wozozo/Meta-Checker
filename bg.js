2617c79d946adb45e5e1f4cb677e226b58350bbc
Yoichi Fujimoto <wozozo@gmail.com>
2025-01-25 15:47:19 +0900

    Migrate to Manifest V3 and modernize JavaScript

--- bg.js
+++ bg.js
@@ -2,24 +2,24 @@
  * @copyright: Richard Parnaby-King
  * @url: http://richard.parnaby-king.co.uk
  */
-var notifications = {};
-chrome.extension.onMessage.addListener(function(msg,sender,sendResponse) {
-	chrome.notifications.create(
-		'', //create new notification id
-		{
-			type: 'basic',
-			iconUrl: 'meta-fail.png',
-			title: 'Meta Robots Checker',
-			message: msg,
-            contextMessage: sender.tab.title,
-			priority: 0,
-            isClickable: true
-		},
-		function(notificationId) {
-            notifications[notificationId] = sender.tab.id;
-        }
-	);
-    chrome.notifications.onClicked.addListener(function(notificationId){
-        chrome.tabs.update(notifications[notificationId], {active: true});
-    });
+const notifications = {};
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  chrome.notifications.create(
+    "", //create new notification id
+    {
+      type: "basic",
+      iconUrl: "meta-fail.png",
+      title: "Meta Robots Checker",
+      message: msg,
+      contextMessage: sender.tab.title,
+      priority: 0,
+      isClickable: true,
+    },
+    (notificationId) => {
+      notifications[notificationId] = sender.tab.id;
+    },
+  );
+  chrome.notifications.onClicked.addListener((notificationId) => {
+    chrome.tabs.update(notifications[notificationId], { active: true });
+  });
 });
--- contentscript.js
+++ contentscript.js
@@ -3,29 +3,28 @@
  * @url: http://richard.parnaby-king.co.uk
  */
 
-//Get all meta tags
-var metaTags=document.getElementsByTagName("meta");
+// Get all meta tags
+const metaTags = document.getElementsByTagName("meta");
 
-//if any meta tags have the attribute name "robots" and either "noindex" or "nofollow" in the attribute contents,
-//then send a message
-//I'm sure there is a way to optimise this loop. Oh well.
-for (var i = 0; i < metaTags.length; i++) {
-	if (metaTags[i].getAttribute("name") == "robots") {
-		var content = metaTags[i].getAttribute("content");
-		var msg = "";
-		if(content.toLowerCase().indexOf('noindex') > -1) {
-			msg = "It looks like this page has a noindex. This means google will not index it";
-		}
+// Check meta tags for robots directives
+for (const metaTag of metaTags) {
+  if (metaTag.getAttribute("name") === "robots") {
+    const content = metaTag.getAttribute("content").toLowerCase();
+    let msg = "";
 
-		if(content.toLowerCase().indexOf('nofollow') > -1) {
-			msg = "It looks like this page has a nofollow. This means google will not crawl the website";
-		}
-		if(content.toLowerCase().indexOf('nofollow') > -1 && content.toLowerCase().indexOf('noindex') > -1) {
-			msg = "It looks like this page has a noindex,nofollow. This means google will not crawl the website or index it";
-		}
+    if (content.includes("noindex") && content.includes("nofollow")) {
+      msg =
+        "It looks like this page has a noindex,nofollow. This means google will not crawl the website or index it";
+    } else if (content.includes("noindex")) {
+      msg =
+        "It looks like this page has a noindex. This means google will not index it";
+    } else if (content.includes("nofollow")) {
+      msg =
+        "It looks like this page has a nofollow. This means google will not crawl the website";
+    }
 
-		if(msg.length > 0) {
-			chrome.extension.sendMessage(msg);
-		}
-	}
+    if (msg) {
+      chrome.runtime.sendMessage(msg);
+    }
+  }
 }
